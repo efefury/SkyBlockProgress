ddabe6e5fb3b37b8202a6bf79f8e3db4a3936310
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2022-10-01 10:27:32 +0200

    added stats deaths

--- src/main/kotlin/at/lorenz/skyblockprogress/compare/CompareData.kt
+++ src/main/kotlin/at/lorenz/skyblockprogress/compare/CompareData.kt
@@ -78,6 +78,7 @@ class CompareData(private val apiKey: String, players: MutableMap<String, String
 
         result.add(makeCompareText("kills", first.kills, second.kills))
         result.addAll(printListChange("stats-kills", first.statsKillsReason, second.statsKillsReason))
+        result.addAll(printListChange("stats-deaths", first.statsDeathsReason, second.statsDeathsReason))
         result.addAll(printListChange("bestiarity-kills", first.bestiarityKills, second.bestiarityKills))
         result.addAll(printListChange("bestiarity-deaths", first.bestiarityDeaths, second.bestiarityDeaths))
 
